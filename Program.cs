db687dafd8c0d903c6744686ed16db24c9fad89c
Tina Shumilova <shutiva@gmail.com>
2021-11-28 16:16:57 +0200

    add my version

--- Program.cs
+++ Program.cs
@@ -1,35 +1,92 @@
-﻿int RequestNumber(string message) 
+﻿string GameInit()
 {
-    Console.WriteLine(message);
-    string line = Console.ReadLine();
-    return Convert.ToInt32(line);
+    string rules = "Добро пожаловать в игру Угадайка!";
+    rules = rules + "Хочешь узнать правила игры?";
+    rules = rules + "Я загадываю число, а ты его угадиваешь :)";
+    return rules;
 }
 
-int CreateSecretNumber(int leftBound, int rightBound) {
-    return new Random().Next(leftBound, rightBound);
-}
-
-bool MakeMove(int secretNumber, int countOfAttempts) 
+(int, int) MimMax()
 {
-    int playersNumber = RequestNumber("У вас осталось " + countOfAttempts + " попыток. " + "Введите число: ");
-    if (playersNumber == secretNumber) 
-    {
-        return true;
-    }
-    if (playersNumber > secretNumber) 
-    {
-        Console.WriteLine("Ваше число больше того, которое мы загадали");
-    } else 
-    {
-        Console.WriteLine("Ваше число меньше того, которое мы загадали");
-    }
-    return false;
-
+    string questions = "Но давай сначала определимся с диапазоном чисел";
+    questions = questions + "Какое число будет минимальным? ";
+    Console.WriteLine(questions);
+    int minNum = requestNumber();
+    Console.Write("А какое максимальным? ");
+    int maxNum = requestNumber();
+    return (minNum, maxNum);
 }
 
-int leftBound = 0;
-int rightBound = 100;
-int countOfAttempts = 7;
-int secretNumber = CreateSecretNumber(leftBound, rightBound);
-// int number = RequestNumber("Введите число: ");
-Console.WriteLine(secretNumber);
+int requestNumber()
+{
+    Console.WriteLine("Введи число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountOfAttempts()
+{
+    Console.WriteLine("Давай ещё определимся с количеством попыток");
+    Console.WriteLine("Сколько попыток у тебя будет? ");
+    return requestNumber();
+}
+
+int CreateNumber(int minNum, int maxNum)
+{
+    return new Random().Next(minNum, maxNum + 1);
+}
+bool IsItTrue(int secretNum, int playerNum)
+{
+    return secretNum == playerNum;
+}
+
+string MoreLess(int secretNum, int playerNum)
+{
+    string answer = String.Empty;
+    if (secretNum > playerNum)
+    {
+        answer = "Неверно! Загаданное число больше твоего :)";
+    }
+    else
+    {
+        answer = "Неверно! Загаданное число меньше твоего :)";
+    }
+    return answer;
+}
+
+int lifes(int secretNum, int count)
+{
+    while (count > 0)
+    {
+        int playerNum = requestNumber();
+        bool arg = IsItTrue(secretNum, playerNum);
+        if (arg == false)
+        {
+            if (secretNum > playerNum)
+            {
+                Console.WriteLine(MoreLess(secretNum, playerNum));
+            }
+            else
+            {
+                Console.WriteLine(MoreLess(secretNum, playerNum));
+            }
+            count--;
+            Console.WriteLine($"У вас осталось {count} попыток");
+        }
+        else
+        {
+            Console.WriteLine("Ура! Победа!");
+            break;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine(GameInit());
+(int minNum, int maxNum) diap = MimMax();
+int count = CountOfAttempts();
+Console.WriteLine($"Итак! Я загадываю число от {diap.minNum} до {diap.maxNum},"
+                + $" а у тебя есть {count} попыток это число отгадать");
+
+int findNum = CreateNumber(diap.minNum, diap.maxNum);
+lifes(findNum, count);
+
