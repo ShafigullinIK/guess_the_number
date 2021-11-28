774a9d2a5d5aa3b708293ca81ca36238785d1905
Tina Shumilova <shutiva@gmail.com>
2021-11-28 17:42:28 +0200

    add checking form Vyacheslav

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿string GameInit()
 {
-    string rules = "Добро пожаловать в игру Угадайка!";
-    rules = rules + "Хочешь узнать правила игры?";
-    rules = rules + "Я загадываю число, а ты его угадиваешь :)";
+    string rules = "Добро пожаловать в игру Угадайка! ";
+    rules = rules + "Хочешь узнать правила игры? ";
+    rules = rules + "Я загадываю число, а ты его угадиваешь :) ";
     return rules;
 }
 
 (int, int) MimMax()
 {
-    string questions = "Но давай сначала определимся с диапазоном чисел";
+    string questions = "Но давай сначала определимся с диапазоном чисел ";
     questions = questions + "Какое число будет минимальным? ";
     Console.WriteLine(questions);
     int minNum = requestNumber();
@@ -19,13 +19,31 @@
 
 int requestNumber()
 {
-    Console.WriteLine("Введи число:");
-    return Convert.ToInt32(Console.ReadLine());
+    int naturalNumber = 0; //переменная integerNumber, в данном случае, 
+                            //хранит число, которое ввел пользователь
+    while (true)
+    {
+        Console.WriteLine("Введи число: ");
+        if (int.TryParse((Console.ReadLine()), out int number)) //извлекаем из строки ввода символы и пытаемся присвоить им вещественный тип числа
+        {
+            if (number > 0)
+            {
+                naturalNumber = number;  //если получилось, запоминаем число
+                Console.Clear();
+                break;          //очищаем экран и выходим из цикла
+            }
+        }
+        else
+        {
+            Console.WriteLine("Не удалось распознать число, попробуй еще раз."); //если не получилось, запрашиваем по новой
+        }
+    }
+    return naturalNumber;        // возвращаем значение метода (функции) в программу
 }
 
 int CountOfAttempts()
 {
-    Console.WriteLine("Давай ещё определимся с количеством попыток");
+    Console.WriteLine("Давай ещё определимся с количеством попыток ");
     Console.WriteLine("Сколько попыток у тебя будет? ");
     return requestNumber();
 }
@@ -44,11 +62,11 @@ string MoreLess(int secretNum, int playerNum)
     string answer = String.Empty;
     if (secretNum > playerNum)
     {
-        answer = "Неверно! Загаданное число больше твоего :)";
+        answer = "Неверно! Загаданное число больше твоего :) ";
     }
     else
     {
-        answer = "Неверно! Загаданное число меньше твоего :)";
+        answer = "Неверно! Загаданное число меньше твоего :) ";
     }
     return answer;
 }
@@ -70,11 +88,11 @@ int lifes(int secretNum, int count)
                 Console.WriteLine(MoreLess(secretNum, playerNum));
             }
             count--;
-            Console.WriteLine($"У вас осталось {count} попыток");
+            Console.WriteLine($"У вас осталось {count} попыток ");
         }
         else
         {
-            Console.WriteLine("Ура! Победа!");
+            Console.WriteLine("Ура! Победа! ");
             break;
         }
     }
@@ -85,7 +103,7 @@ Console.WriteLine(GameInit());
 (int minNum, int maxNum) diap = MimMax();
 int count = CountOfAttempts();
 Console.WriteLine($"Итак! Я загадываю число от {diap.minNum} до {diap.maxNum},"
-                + $" а у тебя есть {count} попыток это число отгадать");
+                + $" а у тебя есть {count} попыток это число отгадать ");
 
 int findNum = CreateNumber(diap.minNum, diap.maxNum);
 lifes(findNum, count);
